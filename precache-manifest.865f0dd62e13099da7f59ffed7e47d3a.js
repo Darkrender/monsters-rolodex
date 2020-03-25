40ae2638d84ec938a0a416050169d289e99d5cd2
Darkrender <jimmiegisclair@gmail.com>
2020-03-25 10:33:50 -0500

    Updates

---
+++ precache-manifest.865f0dd62e13099da7f59ffed7e47d3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed1d8eb28c77c4248b0350ee87ca095",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "187ee4502554fd9ce127",
+    "url": "/monsters-rolodex/static/css/main.6bba88fa.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187ee4502554fd9ce127",
+    "url": "/monsters-rolodex/static/js/main.66a5ac4f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
